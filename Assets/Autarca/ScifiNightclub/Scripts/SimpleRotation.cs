c6b7bcb5931b6912736d4108a0bed79f543e2418
Jean Gustave <zbigmoney305@gmail.com>
2022-01-13 19:13:04 -0600

    Adding Asset files with A

---
+++ Assets/Autarca/ScifiNightclub/Scripts/SimpleRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+
+namespace Autarca
+{
+    public class SimpleRotation : UdonSharpBehaviour
+    {
+        public Vector3 rotation;
+
+        private Transform _transform;
+
+        private void Start()
+        {
+            _transform = transform;
+        }
+
+        public override void PostLateUpdate()
+        {
+            _transform.Rotate(rotation * Time.deltaTime);
+        }
+    }
+}
