704955a3c243820798dd069d0bf8e7070c8e2ce9
Jean Gustave <zbigmoney305@gmail.com>
2022-01-13 18:31:45 -0600

    Delete Assets directory

--- Assets/Okashi/CalenderSystem/Scripts/UdonCalenderWeek.cs
+++
@@ -1,9 +0,0 @@
-﻿using UdonSharp;
-using UnityEngine;
-
-public class UdonCalenderWeek : UdonSharpBehaviour
-{
-    public UdonCalenderDay[] days;
-    public UdonCalenderDay GetDay(int d) => days.Length > 0 ?  days[d] : null;
-    public int GetDayCount() => days != null ? days.Length : 0;
-}
