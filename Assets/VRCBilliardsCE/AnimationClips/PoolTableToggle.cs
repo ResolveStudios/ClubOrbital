49e304c2a95d5d36adb64e7063f4b927481c687c
Jean Gustave <zbigmoney305@gmail.com>
2022-01-13 19:30:44 -0600

    Added vrc item

---
+++ Assets/VRCBilliardsCE/AnimationClips/PoolTableToggle.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PoolTableToggle : UdonSharpBehaviour
+{
+    public Animator anim;
+    [UdonSynced] public bool on;
+
+    public void Update()
+    {
+        if (!anim) anim = (Animator)GetComponent(typeof(Animator));
+
+        var value = anim.GetFloat("Show");
+        value = Mathf.Lerp(value, on ? 1 : 0, Time.deltaTime);
+        anim.SetFloat("Show", value);
+    }
+
+    public void Toggle() => on = !on;
+}
