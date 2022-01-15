5f183728b28bce06f5f1138d5a766cccfd6b3d28
OkashiKami <zbigmoney305@gmail.com>
2022-01-14 20:45:20 -0600

    publish

---
+++ Assets/Okashi/PermissionManager/Scripts/PermissionDoorOpener.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PermissionDoorOpener : UdonSharpBehaviour
+{
+    public bool open;
+    private bool opended;
+    public float curOpen;
+    public float maxOpen = 0.952f;
+    public Transform doorLeft, doorRight;
+
+    private float speed = 1;
+
+    public override void PostLateUpdate()
+    {
+        speed = Mathf.Clamp(speed, 1f, 20f);
+        if(open && curOpen != maxOpen)
+        {
+            speed += Time.deltaTime;
+            curOpen = Mathf.Lerp(curOpen, maxOpen, speed * Time.deltaTime);
+        }
+        else if (!open && curOpen != 0)
+        {
+            speed += Time.deltaTime;
+            curOpen = Mathf.Lerp(curOpen, 0f, speed * Time.deltaTime);
+        }
+        else
+        {
+            speed = 1f;
+        }
+
+        if (doorLeft) doorLeft.localPosition = Vector3.right * curOpen;
+        if (doorRight) doorRight.localPosition = Vector3.right * -curOpen;
+    }
+}
