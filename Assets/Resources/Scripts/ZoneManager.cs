fd278b113e3a094cc192e9c795f54385eaed8f77
OkashiKami <zbigmoney305@gmail.com>
2022-01-15 20:10:17 -0600

    Load Zones
    Added Load zones to the world so that the section loading works, World need to be lightmaped

---
+++ Assets/Resources/Scripts/ZoneManager.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ZoneManager : UdonSharpBehaviour
+{
+    public float UnloadDistance = 100f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, UnloadDistance);
+    }
+
+    private void Update()
+    {
+        var dis = Vector3.Distance(transform.position, Networking.LocalPlayer.GetPosition());
+        transform.GetChild(0).gameObject.SetActive(dis < UnloadDistance);
+    }
+}
