e45a29d99f8fb76650e7e7b9293574edef077707
OkashiKami <zbigmoney305@gmail.com>
2022-01-22 21:15:28 -0600

    Re-Design Meter Stick

--- Assets/Okashi/MeeterStick.cs
+++ Assets/Okashi/MeeterStick.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 [ExecuteAlways]
 public class MeeterStick : MonoBehaviour
 {
-    [Range(1, 12)]
+    [Range(0, 12)]
     public float feet;
-    [Range(1, 12)]
+    [Range(0, 12)]
     public float inches;
     [SerializeField] private float meeters = 1f;
 
     private void Update()
     {
         meeters = (feet + (inches / 12)) / 3.28f;
-        var stick = transform.GetChild(0);
-        stick.localScale = new Vector3(0.1f, meeters, 0.1f);
-        stick.localPosition = Vector3.up * (meeters / 2);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.3f);
+        Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0, 0.5f));
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position,  transform.position + (Vector3.up * meeters));
+        Gizmos.color = new Color(0, 0, 1, 0.3f);
+        Gizmos.DrawCube(transform.position + (Vector3.up * meeters), new Vector3(0.5f, 0, 0.5f));
     }
 }
